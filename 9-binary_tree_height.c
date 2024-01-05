441b7cb734b2a0e8aa42418b0bdfa512cebb3511
teflon40 <frimpong.bernard3220@gmail.com>
2024-01-05 02:06:35 +0100

    Calculates the height of a node

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+
+int height(const binary_tree_t *tree)
+{
+	int hr, hl;
+
+	if (tree == NULL)
+		return (-1);
+
+	hr = height(tree->right);
+	hl = height(tree->left);
+
+	return (Max(hr, hl) + 1);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree.
+ *
+ * @tree: a pointer to the root node of the tree to measure the height.
+ *
+ * Return: 0 if tree is NULL, otherwise the height of the tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	return (tree == NULL ? 0 : height(tree));
+}
