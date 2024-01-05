1f916046c4e0a216a6aae578c4bee143f57239a1
teflon40 <frimpong.bernard3220@gmail.com>
2024-01-05 02:26:16 +0100

    Solved betty error: line over 80 characters

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,8 @@
  * if parent already has a right-child, the new node must take its place,
  * and the old right-child must be set as the right-child of the new node.
  *
- * Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: a pointer to the created node, or NULL on failure,
+ * or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
